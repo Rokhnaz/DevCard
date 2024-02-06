8061ded262f1f517890ba1886453eb9f52d0a21b
Rokhnaz <rokhnaz.mazar@gmail.com>
2024-02-06 18:00:30 +0330

    Projects & latest Articles moved to view component

---
+++ DevCard_Prject/DevCard_MVC/ViewComponents/ArticleViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class LatestArticlesViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var LatestArticles = new List<Article>()
+            {
+                new Article(1, "مقاله اول", "توضیحات مقاله اول", "blog-post-thumb-1.jpg"),
+                new Article(2, "مقاله اول", "توضیحات مقاله اول", "blog-post-thumb-2.jpg"),
+                new Article(3, "مقاله اول", "توضیحات مقاله اول", "blog-post-thumb-3.jpg"),
+                new Article(4, "مقاله اول", "توضیحات مقاله اول", "blog-post-thumb-4.jpg"),
+            };
+            return View("_LatestArticles",LatestArticles);
+        }
+    }
+}
---
+++ DevCard_Prject/DevCard_MVC/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using DevCard_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.ViewComponents
+{
+    public class ProjectsViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var projects = new List<Project>()
+            {
+                new Project(1, "تاکسی", "این یک محصول اول است", "project-1.jpg", "مشتری اول"),
+                new Project(2, "فست فود", "این یک محصول اول است", "project-2.jpg", "مشتری دوم"),
+                new Project(3, "فضا پیما", "این یک محصول اول است", "project-3.jpg", "مشتری سوم"),
+                new Project(4, "اتومبیل", "این یک محصول اول است", "project-4.jpg", "مشتری چهارم"),
+            };
+            return View("_Projects", projects);
+        }
+    }
+}
